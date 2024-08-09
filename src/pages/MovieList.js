5bf75342c4bd327069626e4a440197b7c5a778f3
1Ir-is <mhuynk1005@gmail.com>
2024-08-09 12:53:07 +0700

    feat: Install tailwindcss

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const MovieList = () => {
     return (
         <main>
-            MovieList
+            <div className='text-2xl '>
+                MovieList
+            </div>
         </main>
     )
 }
