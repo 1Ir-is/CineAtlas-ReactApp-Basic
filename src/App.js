52f072eebfc53184cafd474529c93478b626089a
1Ir-is <mhuynk1005@gmail.com>
2024-08-09 14:02:46 +0700

    feat: design UI for header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header, Footer } from './components';
 
 import { AllRoutes } from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer/>
     </div>
   );
 }
