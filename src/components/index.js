86e52ce40246dc849191039b4efc5c66e9aef84b
1Ir-is <mhuynk1005@gmail.com>
2024-08-12 17:07:37 +0700

    feat: create scroll to top function

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { Footer } from './Footer';
 export { Card } from './Card';
 
 export { Button } from './Button';
+export { ScrollToTop } from './ScrollToTop';
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ScrollToTop } from "./components";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
