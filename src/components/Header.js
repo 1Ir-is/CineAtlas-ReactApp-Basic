923aa0972473d7135f95dcb8b4c7ac1e3055053d
1Ir-is <mhuynk1005@gmail.com>
2024-08-12 15:20:13 +0700

    feat: make search function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/irisnet-iris-logo.png";
 
 export const Header = () => {
 
   const [hidden, setHidden] = useState(true);
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -19,13 +20,19 @@ export const Header = () => {
   const activeClass ="text-base block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
   const inactiveClass = "text-base block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const queryTerm = event.target.search.value;
+    event.target.reset();
+    return navigate(`/search?query=${queryTerm}`);
+  }
 
   return (
     <header>
       <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link
-            to="https://flowbite.com/"
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -86,13 +93,17 @@ export const Header = () => {
                 </svg>
                 <span className="sr-only">Search icon</span>
               </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
+              </form>
+              
             </div>
             <button
               onClick={() => setHidden(!hidden)}
@@ -143,13 +154,17 @@ export const Header = () => {
                   />
                 </svg>
               </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
+              </form>
+
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useSearchParams } from "react-router-dom";
 
-export const Search = () => {
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components/Card"
+
+export const Search = ({ apiPath }) => {
+    const [searchParams] = useSearchParams();
+    const queryTerm = searchParams.get("query");
+    const { data : movies } = useFetch(apiPath, queryTerm);
+
     return (
         <main>
-            Search
+            <section className="py-7">
+                <p className="
+                    text-3xl
+                    text-gray-700
+                    text-center
+                    dark:text-white
+                ">
+                    { movies.length > 0 ? `Search results for '${queryTerm}'` : `No results for '${queryTerm}'`}
+                </p>
+            </section>
+            <section className='
+                max-w-7xl
+                mx-auto
+                py-7
+            '>
+                <div className='
+                    flex
+                    justify-start
+                    flex-wrap
+                '>
+                    { movies.map((movie) => (
+                        <Card key={movie.id} movie={movie}/>
+                    ))}             
+                </div>
+            </section>
         </main>
     )
 }
